9a371641980d73c1277aab1083bb5aa3f2e44616
Leonardo Zanaro <109481530+leonardo-zanaro@users.noreply.github.com>
2022-09-18 18:05:11 -0300

    Tratamento de imagem

--- webcam.py
+++ webcam.py
@@ -1,21 +1,19 @@
 import numpy
 import cv2
 
-webcam = cv2.VideoCapture(0)
+img = cv2.imread("images/sample3.png")
 
-aux = 0
+# 2. Resize the image
+img = cv2.resize(img, None, fx=0.5, fy=0.5)
 
-if webcam.isOpened():
-    validacao, frame = webcam.read()
-    while validacao:
-        validacao, frame = webcam.read()
-        cv2.imshow("Captura da webcam", frame)
-        key = cv2.waitKey(10)
-        if key == 27: #ESC
-            break
-        if key == 80 or 112:
-            cv2.imwrite(f"images/image_{aux}.png", frame)
-            aux + 1
+# 3. Convert image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-webcam.release()
-cv2.destroyAllWindows()
+# 4. Convert image to black and white (using adaptive threshold)
+adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 85, 11)
+text = pytesseract.image_to_string(img)
+print(text)
+
+cv2.imshow("gray", gray)
+cv2.imshow("adaptive th", adaptive_threshold)
+cv2.waitKey(0)
